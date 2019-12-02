f8ec29a7b75a404bf6f304d9d55dfb78e0705227
Vladimir <79854322262@yandex.ru>
2019-12-03 00:30:32 +0300

    Сделан каркас таймлайна

--- src/Timeline/Timeline.tsx
+++ src/Timeline/Timeline.tsx
@@ -1,11 +1,29 @@
 import React, { FC } from 'react';
 import './Timeline.scss';
 import TimeUnit from '../TimeUnit/TimeUnit'
+import { cn } from '@bem-react/classname';
+
+const cnTimeline = cn('Timeline');
 
 const Timeline: FC = () => {
+
+    const isDesktop = document.body.clientWidth > 700;
+
+    const makeGrid = (timeUnitValueInMins: number) => {
+        const unitsPerHour = 60 / timeUnitValueInMins;
+        const unitsPerDay = 24 * unitsPerHour;
+        const timeUnits = [];
+
+        for (let i = 0; i < unitsPerDay; i++) {
+            timeUnits.push(<TimeUnit key={i} />);
+        }
+
+        return timeUnits;
+    };
+
     return (
-        <div className="Timeline">
-            <TimeUnit />
+        <div className={isDesktop ? cnTimeline({mobile: true}) : cnTimeline({desktop: true})}>
+            {makeGrid(5)}
         </div>
     );
 };
